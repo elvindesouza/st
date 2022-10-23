41b4b9e7c5998a17c194d5b3e83888f7e23b4ed6
elvindesouza <elvindesouza@gmail.com>
2022-10-23 13:30:32 +0530

    merged

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Term:pixelsize=52:antialias=true:autohint=true";
+static char *font = "Iosevka Term:pixelsize=54:antialias=true:autohint=true";
 #if FONT2_PATCH
 /* Spare fonts */
 static char *font2[] = {
